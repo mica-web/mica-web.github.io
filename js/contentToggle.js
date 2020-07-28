26d7429aa51ee3e561e239eb365a0a8283dfa153
Angelique Weger <hello@angeliqueweger.com>
2020-07-27 22:02:10 -0400

    Add role to SVGs

--- js/contentToggle.js
+++ js/contentToggle.js
@@ -11,7 +11,7 @@
     heading.innerHTML = `
       <button class="u-button-reset" aria-expanded="false">
         ${heading.textContent}
-        <svg aria-hidden="true" focusable="false" viewBox="0 0 10 10">
+        <svg aria-hidden="true" focusable="false" role="img" viewBox="0 0 10 10">
           <rect class="vert" height="8" width="2" y="1" x="4"/>
           <rect height="2" width="8" y="4" x="1"/>
         </svg>
